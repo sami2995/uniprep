106ec999c2cf487b0414f99f59018c0483f63120
SAMIYAS SOLOMON <138712866+sami2995@users.noreply.github.com>
2026-08-05 23:41:39 +0300

    Configure CORS for Vercel

--- uniprep_backend/uniprep_backend/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pip install -r requirements_deploy.txt
-
-python manage.py collectstatic --noinput
-
-python manage.py migrate
--- uniprep_backend/uniprep_backend/settings.py
+++ uniprep_backend/uniprep_backend/settings.py
@@ -95,6 +95,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://uniprep-lvoo6br06-samiyas-projects-02f55793.vercel.app",
+
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://uniprep-lvoo6br06-samiyas-projects-02f55793.vercel.app",
 ]
 
 CHANNEL_LAYERS = {
